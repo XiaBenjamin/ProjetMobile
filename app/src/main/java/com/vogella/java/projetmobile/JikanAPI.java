008b930630e23db1fcc72148660cdf25a414cc15
XiaBenjamin <EdenEternal25>
2019-02-20 14:25:51 +0100

    First commit

---
+++ app/src/main/java/com/vogella/java/projetmobile/JikanAPI.java
@@ -0,0 +1,11 @@
+package com.vogella.java.projetmobile;
+
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public class JikanAPI {
+}
---
+++ app/src/main/java/com/vogella/java/projetmobile/model/Anime.java
@@ -0,0 +1,22 @@
+package com.vogella.java.projetmobile.model;
+
+public class Anime {
+    private String title;
+    private String url;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
