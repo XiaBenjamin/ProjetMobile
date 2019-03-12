e0f10a04b55a0f0279fc8854bdf58c33fef0ec50
XiaBenjamin <EdenEternal25>
2019-03-12 14:32:14 +0100

    Continued on second activity

--- app/src/main/java/com/vogella/java/projetmobile/DetailsActivity.java
+++ app/src/main/java/com/vogella/java/projetmobile/DetailsActivity.java
@@ -2,6 +2,12 @@ package com.vogella.java.projetmobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.squareup.picasso.Picasso;
+import com.vogella.java.projetmobile.model.Anime;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -9,5 +15,14 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        String animeJson = getIntent().getStringExtra("anime");
+        Gson gson = new Gson();
+        Anime json = gson.fromJson(animeJson, Anime.class);
+
+        TextView viewTitle = (TextView) findViewById(R.id.detailsTitle);
+        viewTitle.setText(json.getTitle());
+
+        ImageView viewImage = (ImageView) findViewById(R.id.detailsImage);
+        Picasso.with(getBaseContext()).load(json.getImage_url()).into(viewImage);
     }
 }
--- app/src/main/java/com/vogella/java/projetmobile/model/Anime.java
+++ app/src/main/java/com/vogella/java/projetmobile/model/Anime.java
@@ -1,8 +1,12 @@
 package com.vogella.java.projetmobile.model;
 
 public class Anime {
+    private int rank;
     private String title;
     private String url;
+    private String image_url;
+    private int members;
+    private double score;
 
     public String getTitle() {
         return title;
@@ -12,11 +16,43 @@ public class Anime {
         return url;
     }
 
+    public double getScore() {
+        return score;
+    }
+
     public void setTitle(String title) {
         this.title = title;
     }
 
     public void setUrl(String url) {
         this.url = url;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    public int getMembers() {
+        return members;
+    }
+
+    public void setMembers(int members) {
+        this.members = members;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
     }
 }
